717de92aad3beb56ea08b86d6fbd4f0c5512bb2a
zhengzhengen <1070963419@qq.com>
2018-10-31 11:34:28 +0800

    修改首页

---
+++ xin/js/kimm.js
@@ -0,0 +1,19 @@
+new Vue({
+    el: '#kimm',
+    data: {
+        name: 'KimZheng',
+        job: '前端工程师',
+        email: '1070963419@qq.com',
+        phone: '13682858617',
+        skill:[
+            {name: 'HTML', shuliandu: 'label-info'}, 
+            {name: 'CSS', shuliandu: 'label-info'}, 
+            {name: 'JavaScript', shuliandu: 'label-success'}, 
+            {name: 'Vue.js', shuliandu: 'label-info'}, 
+            {name: 'jQuery', shuliandu: 'label-success'}, 
+            {name: 'Booststrap', shuliandu: 'label-info'}, 
+            {name: 'webpack', shuliandu: 'label-success'},
+            {name: 'Node.js', shuliandu: 'label-default'}
+        ]
+    }
+})
